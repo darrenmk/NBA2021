d14a17b9a02a5f0ffd92c3666220be756a139ad3
Darren <darren.kenney@outlook.com>
2021-03-02 15:09:40 -0500

    small changes to variable names

--- React4_LMM.R
+++ React4_LMM.R
@@ -21,14 +21,11 @@ data$compensation <- factor(data$compensation,levels=c("Money","Credit","Decline
 lmm.rt <- lme(rt ~ age*soa + gameHr + driveHr + compHr, random = ~1|sID, na.action = na.exclude, data = data)
 summary(lmm.rt)
 anova(lmm.rt)
-lmm.rt2 <- lmer(rt ~ age + gameHr + driveHr + compHr + (1|sID), data=data)
-summary(lmm.rt2)
-# sim.rt <- powerSim(lmm.rt2,nsim=1000,seed=1995) # commented out because it takes a while to run, seed added for reproducibility
 
 lmm.mr <- lme(mr ~ age*soa + gameHr + driveHr + compHr, random = ~1|sID, na.action = na.exclude, data = data)
 summary(lmm.mr)
 anova(lmm.mr)
-
+cor(data$age[data$soa==-Inf], data$mr[data$soa==-Inf]) # directly look at correlation
 
 ## Stats for Figure 5
 dataSoa <- data[(data$soa != -Inf) & (data$soa != Inf),]
@@ -38,8 +35,8 @@ dataSoa$soa <- factor(dataSoa$soa,levels=c("-100","-50","0","50","100"))
 lmm.va <- lme(va ~ age*soa + gameHr + driveHr + compHr, random = ~1|sID, na.action = na.exclude, data = dataSoa)
 summary(lmm.va)
 anova(lmm.va)
-summary(lme(va ~ age + gameHr + driveHr + compHr, random=~1|sID, data=dataSoa)) # just to get p-values
-lmm.va2 <- lmer(va ~ age + gameHr + driveHr + compHr + (1|sID), data=dataSoa)
+summary(lme(va ~ age + gameHr + driveHr + compHr, random = ~1|sID, data = dataSoa)) # just to get p-values
+lmm.va2 <- lmer(va ~ age + gameHr + driveHr + compHr + (1|sID), data = dataSoa)
 summary(lmm.va2)
 # sim.va <- powerSim(lmm.va2,nsim=1000,seed=1995) # commented out because it takes a while to run, seed added for reproducibility
 
@@ -65,15 +62,15 @@ anova(comp.me)
 
 # Control analysis 3.3.2. High miss rate in vestibular-only condition (>= 80%)
 vmr <- rep(data$mr[data$soa==-Inf] >= 0.8, each=7) # get subjects with high vestibular MR >= 80%
-vmrVisRT <- data$rt[highVMR & data$soa==Inf] # get vis-only RTs for this subset of participants
-vmrSOART <- t(matrix(data$rt[highVMR & data$soa!=-Inf & data$soa!=Inf],nrow=5)) # get soa RTs this subset...
-vmrSOART[,1] <- vmrSOART[,1] - 100 # adjust for vis 100 ms late
-vmrSOART[,2] <- vmrSOART[,2] - 50 # adjust for vis 50 ms late
-vmrT <- t.test(vmrSOART, vmrVisRT, alternative = "less")
+vmrVis <- data$rt[vmr & data$soa==Inf] # get vis-only RTs for this subset of participants
+vmrSOA <- t(matrix(data$rt[vmr & data$soa!=-Inf & data$soa!=Inf],nrow=5)) # get soa RTs this subset...
+vmrSOA[,1] <- vmrSOA[,1] - 100 # adjust for vis 100 ms late
+vmrSOA[,2] <- vmrSOA[,2] - 50 # adjust for vis 50 ms late
+vmrT <- t.test(vmrSOART, vmrVis, alternative = "less")
 
 
 
-## for reviewer 1: check whether young adults with >50% MR had diff demographic qualities
+## for Reviewer 1: check whether young adults with >50% MR had diff demographic qualities
 data2 <- data[data$soa==-Inf,]
 data2$gameHr <- as.integer(data2$gameHr)
 data2$driveHr <- as.integer(data2$driveHr)
